2330ce4aeba77ce941d8e1a3cafe7b8d5afc5cb6
rikut <riku.tiitta@hotmail.com>
2023-09-06 03:48:28 +0300

    More cleanup and error check

--- src/renderer/Authorize.ts
+++ src/renderer/Authorize.ts
@@ -1,15 +1,15 @@
 import { getAccessToken, redirectToAuthCodeFlow } from 'main/authorize-spotify';
 
 export const AuthorizeUser = () => {
-  const clientId = '5313b75baba342288d5e3a01dba242c0';
+  const clientId = '';
   const params = new URLSearchParams(window.location.search);
   const code = params.get('code');
   if (!code && !localStorage.getItem('access_token')) {
     redirectToAuthCodeFlow(clientId);
   } else {
     if (localStorage.getItem('access_token')) {
-    } else {
-      getAccessToken(clientId, code ?? '');
+    } else if(code) {
+      getAccessToken(clientId, code);
     }
   }
 };
